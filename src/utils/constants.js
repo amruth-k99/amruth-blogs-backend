542f1da6c8a1942265cfef06f0ca0f54941540ab
amruth-kuntamalla <88645369+amruth-kuntamalla@users.noreply.github.com>
2022-10-23 17:31:02 +0530

    cleared unnecessary imports

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,6 +1,5 @@
 module.exports = {
-  MONGO_URI:
-    "mongodb+srv://amruth:yBlI0tlAqWL2NEir@studentportal.ioyau.mongodb.net/amruth_blogs",
+  MONGO_URI: process.env.MONGO_URI,
   DB_NAME: "amruth_blogs",
   POSTS_PAGE_LIMIT: 10,
   COMMENTS_PAGE_LIMIT: 20,
